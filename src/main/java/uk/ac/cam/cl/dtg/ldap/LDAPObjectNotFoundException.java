cbfe56cf9f2f8e710ee7364be8f2356aaaac2c84
Andrew Rice <acr31@cam.ac.uk>
2013-10-18 10:43:44 +0100

    Added extra constructors to exception so I can chain causes

--- src/main/java/uk/ac/cam/cl/dtg/ldap/LDAPObjectNotFoundException.java
+++ src/main/java/uk/ac/cam/cl/dtg/ldap/LDAPObjectNotFoundException.java
@@ -10,4 +10,26 @@ public class LDAPObjectNotFoundException extends Exception {
 	public LDAPObjectNotFoundException(String message) {
 		super(message);
 	}
+
+	public LDAPObjectNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public LDAPObjectNotFoundException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LDAPObjectNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LDAPObjectNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
 }
