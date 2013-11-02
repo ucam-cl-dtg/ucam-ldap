ucam-cl-dtg/ucam-ldap

commit 30168b9b8a229bf16a95557ff2298693925b1e80

Tidied up LDAPProvider in preparation for adding some new features

src/main/java/uk/ac/cam/cl/dtg/ldap/LDAPProvider.java
@@ -1,19 +1,21 @@
 package uk.ac.cam.cl.dtg.ldap;
 
-import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 
-import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Holly Priest <hp343@cam.ac.uk>
@@ -27,6 +29,16 @@ public class LDAPProvider {
 	private static final String PROVIDER_URL = "ldap://ldap.lookup.cam.ac.uk:389";
 	private static final String CONTEXT_FILTER = "o=University of Cambridge,dc=cam,dc=ac,dc=uk";
 
+	private static final Logger log = LoggerFactory
+			.getLogger(LDAPProvider.class);
+
+	private static final Hashtable<String,String> env;
+	static {
+		env = new Hashtable<String, String>();
+		env.put(Context.INITIAL_CONTEXT_FACTORY, CONTEXT_FACTORY);
+		env.put(Context.PROVIDER_URL, PROVIDER_URL);
+	}
+	
 	/**
 	 * Initialises context and sets up basic query
 	 * 
@@ -34,36 +46,24 @@ public class LDAPProvider {
 	 */
 	private static NamingEnumeration<SearchResult> initialiseContext(
 			String type, String parameter, String subtree, boolean partial) {
-
-		Hashtable<String, String> env = new Hashtable<String, String>();
-		env.put(Context.INITIAL_CONTEXT_FACTORY, CONTEXT_FACTORY);
-		env.put(Context.PROVIDER_URL, PROVIDER_URL);
-
-		String searchContext = "ou=" + subtree + "," + CONTEXT_FILTER;
-		String searchParameters;
-		if (partial) {
-			searchParameters = "(" + type + "=" + parameter + "*)";
-		} else {
-			searchParameters = "(" + type + "=" + parameter + ")";
-		}
-
-		DirContext ctx;
-		NamingEnumeration<SearchResult> searchResults;
 		try {
-
-			ctx = new InitialDirContext(env);
-
+			DirContext ctx = new InitialDirContext(env);
 			SearchControls controls = new SearchControls();
 			controls.setSearchScope(SearchControls.SUBTREE_SCOPE);
-			searchResults = ctx.search(searchContext, searchParameters,
-					controls);
+
+			String searchContext = "ou=" + subtree + "," + CONTEXT_FILTER;
+			String searchParameters = "(" + type + "=" + parameter
+					+ (partial ? "*" : "") + ")";
+			NamingEnumeration<SearchResult> searchResults = ctx.search(
+					searchContext, searchParameters, controls);
+
+			return searchResults;
 
 		} catch (NamingException e) {
-			return null;
+			log.warn("Failed to initialise LDAP context", e);
 		}
 
-		return searchResults;
-
+		return new EmptyNamingEnumeration();
 	}
 
 	/**
@@ -74,23 +74,20 @@ public class LDAPProvider {
 	 */
 	private static Attributes setupUniqueQuery(String type, String parameter,
 			String subtree) throws LDAPObjectNotFoundException {
-		SearchResult searchResult;
-
 		try {
-			searchResult = initialiseContext(type, parameter, subtree, false)
-					.next();
-
+			SearchResult searchResult = initialiseContext(type, parameter, subtree,
+					false).next();
 			if (searchResult == null) {
+				log.warn(
+						"No result found for query type={}, parameter={},subtree={}",
+						type, parameter, subtree);
 				return null;
 			}
-
+			return searchResult.getAttributes();
 		} catch (NamingException e) {
+			log.warn("Naming exception when processing NamingEnumeration",e);
 			return null;
-		} catch (NullPointerException e) {
-			throw new LDAPObjectNotFoundException("User not found");
 		}
-
-		return searchResult.getAttributes();
 	}
 
 	/**
@@ -99,10 +96,10 @@ public class LDAPProvider {
 	 * 
 	 * @return LDAPUser
 	 */
-	static LDAPUser uniqueUserQuery(String type, String parameter)
+	static LDAPUser uniqueUserQuery(String lookupKey, String lookupValue)
 			throws LDAPObjectNotFoundException {
 
-		Attributes userResult = setupUniqueQuery(type, parameter, "people");
+		Attributes userResult = setupUniqueQuery(lookupKey, lookupValue, "people");
 
 		if (userResult == null) {
 			throw new LDAPObjectNotFoundException("User not found");
@@ -114,20 +111,20 @@ public class LDAPProvider {
 	/**
 	 * List of users Query Returns a list of LDAPUser objects
 	 */
-	static ArrayList<LDAPUser> multipleUserQuery(String type, String parameter,
+	static List<LDAPUser> multipleUserQuery(String type, String parameter,
 			boolean partial) throws LDAPObjectNotFoundException {
 
 		NamingEnumeration<SearchResult> searchResults = initialiseContext(type,
 				parameter, "people", partial);
 
-		ArrayList<LDAPUser> users = new ArrayList<LDAPUser>();
-
+		List<LDAPUser> users = new LinkedList<LDAPUser>();
 		try {
 			while (searchResults.hasMore()) {
 				users.add(initLDAPUser(searchResults.next().getAttributes()));
 			}
 		} catch (NamingException e) {
-			return null;
+			log.warn("Naming exception when processing NamingEnumeration in multipleUserQuery",e);
+			return new LinkedList<LDAPUser>();
 		}
 
 		return users;
@@ -145,6 +142,10 @@ public class LDAPProvider {
 
 		Attributes groupResult = setupUniqueQuery(type, parameter, "groups");
 
+		if (groupResult == null) {
+			throw new LDAPObjectNotFoundException("Group not found");
+		}
+		
 		return initLDAPGroup(groupResult);
 	}
 
@@ -161,25 +162,54 @@ public class LDAPProvider {
 		NamingEnumeration<SearchResult> searchResults = initialiseContext(type,
 				parameter, "groups", partial);
 
-		ArrayList<LDAPGroup> groups = new ArrayList<LDAPGroup>();
-
 		try {
+			List<LDAPGroup> groups = new LinkedList<LDAPGroup>();
 			while (searchResults.hasMore()) {
 				try {
 					LDAPGroup g = initLDAPGroup(searchResults.next()
 							.getAttributes());
 					groups.add(g);
 				} catch (LDAPObjectNotFoundException e) {
+					// I think we do this because some groups might not be visible
+					// TODO: just update the search to only include visible groups
 					// don't add the group to the list
 					// log.debug(e.getMessage);
 				}
 			}
+			return groups;
 		} catch (NamingException e) {
-			return null;
+			log.warn("Naming exception in multipleGroupQuery",e);
+			return new LinkedList<LDAPGroup>();
 		}
+	}
 
-		return groups;
+	private static String getString(Attributes userResult, String name) {
+		Attribute a = userResult.get("name");
+		if (a != null) {
+			try {
+				return a.get().toString();
+			} catch (NamingException e) {
+				log.warn("Naming exception looking for LDAP result", e);
+			}
+		}
+		return null;
+	}
 
+	private static List<String> getStringList(Attributes userResult, String name) {
+		Attribute a = userResult.get("name");
+		if (a != null) {
+			try {
+				NamingEnumeration<?> instIDEnum = a.getAll();
+				LinkedList<String> instID = new LinkedList<String>();
+				while (instIDEnum.hasMore()) {
+					instID.add(instIDEnum.next().toString());
+				}
+				return instID;
+			} catch (NamingException e) {
+				log.warn("Naming exception looking for LDAP result", e);
+			}
+		}
+		return null;
 	}
 
 	/**
@@ -190,119 +220,23 @@ public class LDAPProvider {
 	private static LDAPUser initLDAPUser(Attributes userResult)
 			throws LDAPObjectNotFoundException {
 
-		String crsid;
-		String cn;
-		String dn;
-		String sn;
-		String mail;
-		List<String> instID;
-		List<String> misAff;
-		List<String> institutions;
-		List<String> photos;
-
-		try {
-			// Get crisd
-			if (userResult.get("uid") != null) {
-				crsid = userResult.get("uid").get().toString();
-			} else {
-				crsid = null;
-			}
-			
-
-			if (userResult.get("cn") != null) {
-				// Get registered name
-				cn = userResult.get("cn").get().toString();
-			} else {
-				cn = null;
-			}
-			
-			if (userResult.get("displayName") != null) {
-				// Get display name
-				dn = userResult.get("displayName").get().toString();
-			} else {
-				dn = null;
-			}
-
-			if (userResult.get("sn") != null) {
-				// Get surname
-				sn = userResult.get("sn").get().toString();
-			} else {
-				sn = null;
-			}
-
-			if (userResult.get("mail") != null) {
-				// Get email
-				mail = userResult.get("mail").get().toString();
-			} else {
-				mail = null;
-			}
-			
-			// Get instID
-			NamingEnumeration<?> instIDEnum;
-			instID = new ArrayList<String>();
-
-			if (userResult.get("instID") != null) {
-				instIDEnum = userResult.get("instID").getAll();
-
-				while (instIDEnum.hasMore()) {
-					instID.add(instIDEnum.next().toString());
-				}
-			} else {
-				instID = null;
-			}
-
-			// Get misAffiliation
-			NamingEnumeration<?> misAffEnum;
-			misAff = new ArrayList<String>();
-
-			if (userResult.get("misAffiliation") != null) {
-				misAffEnum = userResult.get("misAffiliation").getAll();
-
-				while (misAffEnum.hasMore()) {
-					misAff.add(misAffEnum.next().toString());
-				}
-			} else {
-				misAff = null;
-			}
-
-			// Get institutions
-			NamingEnumeration<?> instEnum;
-			institutions = new ArrayList<String>();
-
-			if (userResult.get("ou") != null) {
-				instEnum = userResult.get("ou").getAll();
-
-				while (instEnum.hasMore()) {
-					institutions.add(instEnum.next().toString());
-				}
-			} else {
-				institutions = null;
-			}
-
-			// Get photos
-			NamingEnumeration<?> photoEnum;
-			photos = new ArrayList<String>();
-
-			if (userResult.get("jpegPhoto") != null) {
-				photoEnum = userResult.get("jpegPhoto").getAll();
-
-				while (photoEnum.hasMore()) {
-					byte[] p = (byte[]) photoEnum.next();
-					photos.add(new String(Base64.encodeBase64(p)));
-				}
-			} else {
-				photos = null;
-			}
-
-		} catch (NamingException e) {
-			return null;
-		}
+		String crsid = getString(userResult, "uid");
 
 		if (crsid == null) { // If uid is null the user does not exist
 			throw new LDAPObjectNotFoundException("User does not exist");
 		}
 
-		return new LDAPUser(crsid, cn, dn, sn, mail, instID, misAff, institutions, photos);
+		String cn = getString(userResult, "cn");
+		String dn = getString(userResult, "displayName");
+		String sn = getString(userResult, "sn");
+		String mail = getString(userResult, "mail");
+		List<String> instID = getStringList(userResult, "instID");
+		List<String> misAff = getStringList(userResult, "misAffiliation");
+		List<String> institutions = getStringList(userResult, "ou");
+		List<String> photos = getStringList(userResult, "jpegPhoto");
+
+		return new LDAPUser(crsid, cn, dn, sn, mail, instID, misAff,
+				institutions, photos);
 
 	}
 
@@ -314,58 +248,19 @@ public class LDAPProvider {
 	private static LDAPGroup initLDAPGroup(Attributes groupResult)
 			throws LDAPObjectNotFoundException {
 
-		String groupID;
-		String groupTitle;
-		String description;
-		List<String> users;
-
-		try {
-
-			if (!groupResult.get("visibility").get().toString().equals("cam")) {
-				throw new LDAPObjectNotFoundException(
-						"Group not publicly visible");
-			}
-
-			if (groupResult.get("groupID") != null) {
-				// Get groupID
-				groupID = groupResult.get("groupID").get().toString();
-			} else {
-				groupID = null;
-			}
-
-			if (groupResult.get("groupTitle") != null) {
-				// Get group name
-				groupTitle = groupResult.get("groupTitle").get().toString();
-				System.out.println(groupTitle);
-			} else {
-				groupTitle = null;
-			}
-
-			if (groupResult.get("description") != null) {
-				// Get description
-				description = groupResult.get("description").get().toString();
-			} else {
-				description = null;
-			}
-
-			// Get users
-			NamingEnumeration<?> usersEnum;
-			users = new ArrayList<String>();
-
-			if (groupResult.get("uid") != null) {
-				usersEnum = groupResult.get("uid").getAll();
-				while (usersEnum.hasMore()) {
-					users.add(usersEnum.next().toString());
-				}
-			}
-
-			if (groupID == null) {
-				throw new LDAPObjectNotFoundException("Group does not exist");
-			}
-
-		} catch (NamingException e) {
-			return null;
+		String visibility = getString(groupResult, "visibility");
+		if (!visibility.equals("cam")) {
+			throw new LDAPObjectNotFoundException("Group not publicly visible");
 		}
+
+		String groupID = getString(groupResult, "groupID");
+		if (groupID == null) {
+			throw new LDAPObjectNotFoundException("Group does not exist");
+		}
+
+		String groupTitle = getString(groupResult, "groupTitle");
+		String description = getString(groupResult, "description");
+		List<String> users = getStringList(groupResult, "uid");
 
 		return new LDAPGroup(groupID, groupTitle, description, users);
 

src/main/java/uk/ac/cam/cl/dtg/ldap/EmptyNamingEnumeration.java
@@ -1,0 +1,32 @@
+package uk.ac.cam.cl.dtg.ldap;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.SearchResult;
+
+final class EmptyNamingEnumeration implements
+		NamingEnumeration<SearchResult> {
+	@Override
+	public boolean hasMoreElements() {
+		return false;
+	}
+
+	@Override
+	public SearchResult nextElement() {
+		return null;
+	}
+
+	@Override
+	public SearchResult next() throws NamingException {
+		return null;
+	}
+
+	@Override
+	public boolean hasMore() throws NamingException {
+		return false;
+	}
+
+	@Override
+	public void close() throws NamingException {
+	}
+}

commit add1b0db15e2ba734fd3e9e03f9c7833b5876f71

Restored code to Base64 encode byte[] arrays when extracted.  This fixes a regression I introduced in https://github.com/ucam-cl-dtg/ucam-ldap/commit/30168b9b8a229bf16a95557ff2298693925b1e80#diff-716391a43d9c992f8fe53d0b0b8c005e

src/main/java/uk/ac/cam/cl/dtg/ldap/LDAPProvider.java
@@ -14,6 +14,7 @@ import javax.naming.directory.InitialDirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -209,7 +210,13 @@ public class LDAPProvider {
 				NamingEnumeration<?> instIDEnum = a.getAll();
 				LinkedList<String> instID = new LinkedList<String>();
 				while (instIDEnum.hasMore()) {
-					instID.add(instIDEnum.next().toString());
+					Object v = instIDEnum.next();
+					if (v instanceof byte[]) {
+						instID.add(new String(Base64.encodeBase64((byte[])v)));
+					}
+					else {
+						instID.add(v.toString());
+					}
 				}
 				return instID;
 			} catch (NamingException e) {